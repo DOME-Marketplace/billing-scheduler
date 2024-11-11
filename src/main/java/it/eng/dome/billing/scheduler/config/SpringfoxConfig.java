1f57589e551576dbd932cceb198b7dbf2d81ae8c
pasquy <pasquale.vitale@gmail.com>
2024-11-11 11:20:50 +0100

    Add swagger feature

---
+++ src/main/java/it/eng/dome/billing/scheduler/config/SpringfoxConfig.java
@@ -0,0 +1,32 @@
+package it.eng.dome.billing.scheduler.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringfoxConfig {
+	
+	private final String TITLE = "Billing Scheduler";
+	private final String DESCRIPTION = "Swagger REST APIs for the billing-scheduler software";
+	
+	@Autowired
+    private BuildProperties buildProperties;
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		
+		String version = buildProperties.getVersion();
+
+        return new OpenAPI()
+                .info(new Info().title(TITLE)
+                .description(DESCRIPTION)
+                .version(version));
+    }
+
+}
--- src/main/java/it/eng/dome/billing/scheduler/controller/BillingSchedulerController.java
+++ src/main/java/it/eng/dome/billing/scheduler/controller/BillingSchedulerController.java
@@ -1,11 +1,22 @@
 package it.eng.dome.billing.scheduler.controller;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,25 +24,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/scheduler")
 public class BillingSchedulerController {
 
-    private static final Logger log = LoggerFactory.getLogger(BillingSchedulerController.class);
+	private static final Logger log = LoggerFactory.getLogger(BillingSchedulerController.class);
 
-    @Value("${application.name}")
-    private String appName;
+    @Autowired
+    private BuildProperties buildProperties;
 
-    @Value("${build.version}")
-    private String buildVersion;
-
-    @Value("${build.timestamp}")
-    private String buildTimestamp;
-
-    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json")
+    @Operation(responses = {
+            @ApiResponse(
+                content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(value = "{\"name\":\"Billing Scheduler\", \"version\":\"0.0.2\", \"release_time\":\"11-11-2024 11:09:51\"}")
+                ))
+        })
     public Map<String, String> getInfo() {
         log.info("Request getInfo");
         Map<String, String> map = new HashMap<String, String>();
-        map.put("name", appName);
-        map.put("version", buildVersion);
-        map.put("timestamp", buildTimestamp);
+        map.put("version", buildProperties.getVersion());
+        map.put("name", buildProperties.getName());
+        map.put("release_time", getFormatterTimestamp(buildProperties.getTime()));
         log.debug(map.toString());
         return map;
+    }
+	
+    private String getFormatterTimestamp(Instant time) {
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        ZonedDateTime zonedDateTime = time.atZone(ZoneId.of("Europe/Rome"));
+    	return zonedDateTime.format(formatter);
     }
 }
