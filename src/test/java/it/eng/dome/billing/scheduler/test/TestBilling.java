4ead5973c0887d1d8070e150cebc08bffd945928
pasquy <pasquale.vitale@gmail.com>
2024-12-19 13:19:55 +0100

    improve algorithm

--- src/test/java/it/eng/dome/billing/scheduler/test/TestBilling.java
+++ src/test/java/it/eng/dome/billing/scheduler/test/TestBilling.java
@@ -5,7 +5,11 @@ import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import it.eng.dome.tmforum.tmf620.v4.ApiException;
 import it.eng.dome.tmforum.tmf620.v4.api.ProductOfferingPriceApi;
@@ -31,7 +35,7 @@ public class TestBilling {
 	private static String tmf637ProductInventoryPath = "tmf-api/productInventory/v4";
 	private static String tmf620CatalgPath = "tmf-api/productCatalogManagement/v4";
 	
-	static OffsetDateTime now = OffsetDateTime.parse("2024-10-12T11:04:38.983Z"); // OffsetDateTime.now();
+	static OffsetDateTime now = OffsetDateTime.parse("2024-08-17T10:04:30.983Z"); // OffsetDateTime.now();
 	
 	public static void main(String[] args) {
 		try {
@@ -40,45 +44,100 @@ public class TestBilling {
 					
 			ProductApi productApi = new ProductApi(apiClient);
 			List<Product> products = productApi.listProduct(/*"id,name"*/ null, null, null);
+			System.out.println("---->>>> Number of Product found = " + products.size());
 			
 			int count = 0;
 			for (Product product : products) {
 				System.out.println("----------------------------------------------------------");
 				System.out.println("item # " + ++count);
+				System.out.println("product - " + product.getName());
 				//System.out.println("product id: " + product.getId() + " - " + product.getName() + " -> STATUS: " + product.getStatus());
 				
 				if (product.getStatus() == ProductStatusType.ACTIVE) {
 					// System.out.println("product id active: " + product.getId()); 
 					List<ProductPrice> pprices = product.getProductPrice();
+					int countPp = 0;
+					
+					Map<String, List<TimePeriod>> timePeriods = new HashMap<>();
+					Map<String, List<ProductPrice>> productPrices = new HashMap<>();
+					
 					for (ProductPrice pprice : pprices) {
-						System.out.println(">> ProductPrice: " + pprice.getName());		
-						System.out.println("calcolare se effettuare il bill verificando la scadenza ... ");
+						//System.out.println(">> ProductPrice: " + pprice.getName());		
+						//System.out.println("checking productPrice ... ");
 						if ("recurring".equals(pprice.getPriceType().toLowerCase()))  {
-							if (pprice.getProductOfferingPrice() != null) {
-								// check on ProductOfferingPrice 
+							//System.out.println("priceType = recurring for productPrice");
+							System.out.println("productPrice # " + ++countPp);
+							
+							
+							String recurringPeriod = null;
+							
+							if (pprice.getProductOfferingPrice() != null) {// check on ProductOfferingPrice 								
 								// GET recurringChargePeriodType + recurringChargePeriodLength
-								System.out.println("ProductOfferingPrice recurring .... " + product.getId());
-								System.out.println(pprice.getProductOfferingPrice().getId());
-								int period = getPeriod(pprice.getProductOfferingPrice().getId());
-								System.out.println("period: " + period);
-								if ((period > 0) && isExpiredTime(product.getStartDate(), period)) {
-									System.out.println("ProductOfferingPrice product for billing .... " + product.getId());
+								System.out.println("Use Case - ProductOfferingPrice for product: " + product.getId());	
+								recurringPeriod = getRecurringPeriod( pprice.getProductOfferingPrice().getId());
+								System.out.println("Recurring period: " + recurringPeriod);
+
+							}else if (pprice.getRecurringChargePeriod() != null) {// check on RecurringChargePeriod									
+								System.out.println("Use Case - RecurringChargePeriod for product: " + product.getId());
+								recurringPeriod = pprice.getRecurringChargePeriod();
+								System.out.println("Recurring period: " + recurringPeriod);								
+							}
+							
+							if (recurringPeriod != null && product.getStartDate() != null) {
+								OffsetDateTime nextBillingTime = getNextBillingTime(product.getStartDate(), recurringPeriod);
+								OffsetDateTime previousBillingTime = getPreviousBillingTime(nextBillingTime, recurringPeriod);
+								
+								if (nextBillingTime != null) {
+									System.out.println("StartDate: " + product.getStartDate());
+									System.out.println("recurring: " + recurringPeriod);
+									System.out.println("NextDate: " + nextBillingTime);
+									System.out.println("PreviuosDate: " + previousBillingTime);
+									
+									
+									System.out.println("CurrentDate: " + now);
+									long days = ChronoUnit.DAYS.between(now, nextBillingTime);
+									System.out.println(">>>days missing for billing: " + days);
+									System.out.println("diff: " + ChronoUnit.DAYS.between(previousBillingTime, nextBillingTime));
+									String keyPeriod = "period-" + ChronoUnit.DAYS.between(previousBillingTime, nextBillingTime);
+									if (days == 0) {
+										TimePeriod tp = new TimePeriod();
+										tp.setStartDateTime(previousBillingTime);
+										tp.setEndDateTime(nextBillingTime);
+										System.out.println("AppliedCustomerBillingRate: " + product.getId() + " " + pprice.getName());
+										//System.out.println(tp);
+										//System.out.println("add: " + keyPeriod + " - obj " + tp);
+										
+										timePeriods.put(keyPeriod, new ArrayList<>(Arrays.asList(tp)));
+										productPrices.computeIfAbsent(keyPeriod, k -> new ArrayList<>()).add(pprice);
+									}
 								}
-							}else if (pprice.getRecurringChargePeriod() != null) {// check on RecurringChargePeriod
-															
-								//get number of days								
-								int period = getNumberOfDays(pprice.getRecurringChargePeriod());
-								System.out.println(":::: " + period + " -> " + pprice.getRecurringChargePeriod());
-								if ((period > 0) && isExpiredTime(product.getStartDate(), period)) {
-									System.out.println("RecurringChargePeriod product for billing .... " + product.getId());
-									TimePeriod tp = new TimePeriod();
-									tp.setStartDateTime(now);
-									tp.setEndDateTime(now.plusDays(period));
-									System.out.println("AppliedCustomerBillingRate: " + product.getId() + " " + pprice.getName());
-									System.out.println(tp);
-								}
-							}							
+							}else {
+								System.out.println("No recurringPeriod found or product.startDate valid");
+							}
 						}
+					}
+					System.out.println("FINE\n");
+					
+					//System.out.println(timePeriods);
+					//System.out.println(productPrices);
+					for (Map.Entry<String, List<ProductPrice>> entry : productPrices.entrySet()) {
+						
+						String key = entry.getKey();
+						System.out.println("key: " + key);
+						//
+						//System.out.println(pp.getName());
+						//List<TimePeriod> tps = timePeriods.get(key);
+						TimePeriod tp = timePeriods.get(key).get(0);
+						if (timePeriods.get(key).size() > 0) {
+							System.out.println("startDate: " + tp.getStartDateTime() + " - endDate: " + tp.getEndDateTime());
+							List<ProductPrice> pps = entry.getValue();
+							for (ProductPrice pp : pps) {
+								System.out.println(pp.getName() + " || " + pp.getPriceType());
+							}
+							System.out.println("Apply billing - AppliedCustomerBillingRate: " + product.getId());
+						}
+						
+						
 					}
 				}
 			}
@@ -106,9 +165,41 @@ public class TestBilling {
 		}
 	}
 	
-	private static boolean isExpiredTime(OffsetDateTime start, int period) {
+	private static String getRecurringPeriod(String id) {
+		final it.eng.dome.tmforum.tmf620.v4.ApiClient apiClient2 = it.eng.dome.tmforum.tmf620.v4.Configuration.getDefaultApiClient();;
+		apiClient2.setBasePath(tmfEndpoint + "/" + tmf620CatalgPath);
+		ProductOfferingPriceApi poffering = new ProductOfferingPriceApi(apiClient2);
+		try {
+			ProductOfferingPrice pop = poffering.retrieveProductOfferingPrice(id, null);
+			//System.out.println("--->>>" + pop.getName() + " >> " + pop.getRecurringChargePeriodType() +" " + pop.getRecurringChargePeriodLength());
+			//System.out.println("num days: " + getNumberOfDays(pop.getRecurringChargePeriodType()));
+			return pop.getRecurringChargePeriodLength() + " " + pop.getRecurringChargePeriodType();
+		} catch (ApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private static OffsetDateTime getNextBillingTimeProductOfferingPrice(OffsetDateTime t, String id) {
+		final it.eng.dome.tmforum.tmf620.v4.ApiClient apiClient2 = it.eng.dome.tmforum.tmf620.v4.Configuration.getDefaultApiClient();;
+		apiClient2.setBasePath(tmfEndpoint + "/" + tmf620CatalgPath);
+		ProductOfferingPriceApi poffering = new ProductOfferingPriceApi(apiClient2);
+		try {
+			ProductOfferingPrice pop = poffering.retrieveProductOfferingPrice(id, null);
+			System.out.println("--->>>" + pop.getName() + " >> " + pop.getRecurringChargePeriodType() +" " + pop.getRecurringChargePeriodLength());
+			//System.out.println("num days: " + getNumberOfDays(pop.getRecurringChargePeriodType()));
+			return nextBillingTime(t, pop.getRecurringChargePeriodLength(), pop.getRecurringChargePeriodType());
+		} catch (ApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private static boolean isBillingTime(OffsetDateTime start, int period) {
 		
-		System.out.println("check isExpiredTime: " + start + " " + now);
+		//System.out.println("check isBillingTime : " + start + " " + now);
 		long days = ChronoUnit.DAYS.between(start, now);
 		System.out.println("Difference in days: " + days);
 		if (days > 0 && days % period == 0) {
@@ -152,6 +243,92 @@ public class TestBilling {
 	        default:
 	            return 0;
 	    }
+	}
+	
+	
+	private static OffsetDateTime getNextBillingTime(OffsetDateTime t, String s) {
+		String[] data = s.split("\\s+");
+		if (data.length == 2) {
+			if (data[0].matches("-?\\d+")) { // if data[0] is a number
+				return nextBillingTime(t, Integer.parseInt(data[0]),  data[1]);
+			}
+		}else if (data.length == 1) {
+			return nextBillingTime(t, 1, data[0]);
+		}		
+		return null;
+	}
+
+	private static String getUnit(String s) {
+		String[] data = s.split("\\s+");
+		if (data.length == 2) {
+			if (data[0].matches("-?\\d+")) { // if data[0] is a number
+				return data[1];
+			}
+		}else if (data.length == 1) {
+			return data[0];
+		}
+		return null;
+	}
+	
+	private static OffsetDateTime getPreviousBillingTime(OffsetDateTime t, String s) {
+		String[] data = s.split("\\s+");
+		if (data.length == 2) {
+			if (data[0].matches("-?\\d+")) { // if data[0] is a number
+				return getPreviusBilling(t, Integer.parseInt(data[0]),  data[1]);
+			}
+		}else if (data.length == 1) {
+			return getPreviusBilling(t, 1, data[0]);
+		}		
+		return null;
+	}
+	
+	private static OffsetDateTime getPreviusBilling(OffsetDateTime time, int number, String unit) {
+		switch (unit) {
+			case "day":
+	        case "days":
+	        case "daily":
+	            return time.minusDays(number);
+	        case "week":
+	        case "weeks":
+	        case "weekly":
+	            return time.minusWeeks(number);
+	        case "month":
+	        case "months":
+	        case "monthly":
+	            return time.minusMonths(number);
+	        case "year":
+	        case "years":
+	            return time.minusYears(number); 
+	        default:
+	            return null;
+	    }
+	}
+	
+	private static OffsetDateTime nextBillingTime(OffsetDateTime time, int number, String unit) {
+		if (time.isAfter(now)) {
+			return time;
+		}else {
+		
+			switch (unit) {
+				case "day":
+		        case "days":
+		        case "daily":
+		            return nextBillingTime(time.plusDays(number), number, unit);
+		        case "week":
+		        case "weeks":
+		        case "weekly":
+		            return nextBillingTime(time.plusWeeks(number), number, unit);
+		        case "month":
+		        case "months":
+		        case "monthly":
+		            return nextBillingTime(time.plusMonths(number), number, unit);
+		        case "year":
+		        case "years":
+		            return nextBillingTime(time.plusYears(number), number, unit);
+		        default:
+		            return null;
+		    }
+		}
 	}
 
 	public static void main1(String[] args) {
