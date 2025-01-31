8595d14bbaaa90d44e8d87c5fc61920c40c12130
pasquy <pasquale.vitale@gmail.com>
2025-01-31 18:43:32 +0100

    Add DateUtils dependency

--- src/main/java/it/eng/dome/billing/scheduler/controller/InfoSchedulerController.java
+++ src/main/java/it/eng/dome/billing/scheduler/controller/InfoSchedulerController.java
@@ -1,15 +1,14 @@
 package it.eng.dome.billing.scheduler.controller;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,9 +16,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import it.eng.dome.brokerage.billing.utils.DateUtils;
 
 @RestController
 @RequestMapping("/scheduler")
@@ -38,15 +35,9 @@ public class InfoSchedulerController {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("version", buildProperties.getVersion());
 		map.put("name", buildProperties.getName());
-		map.put("release_time", getFormatterTimestamp(buildProperties.getTime()));
+		map.put("release_time", DateUtils.getFormatterTimestamp(buildProperties.getTime()));
 		log.debug(map.toString());
 		return map;
-	}
-
-	private String getFormatterTimestamp(Instant time) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-		ZonedDateTime zonedDateTime = time.atZone(ZoneId.of("Europe/Rome"));
-		return zonedDateTime.format(formatter);
 	}
 
 	private final String INFO = "{\"name\":\"Billing Scheduler\", \"version\":\"0.0.2\", \"release_time\":\"11-11-2024 11:09:51\"}";
