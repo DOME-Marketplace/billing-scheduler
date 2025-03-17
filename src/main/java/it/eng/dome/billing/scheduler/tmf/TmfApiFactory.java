cd85f6b18747df82863376562dbd6dac070a3bda
pasquy <pasquale.vitale@gmail.com>
2025-03-17 14:34:07 +0100

    Improvment TMF-API

--- src/main/java/it/eng/dome/billing/scheduler/tmf/TmfApiFactory.java
+++ src/main/java/it/eng/dome/billing/scheduler/tmf/TmfApiFactory.java
@@ -15,7 +15,7 @@ import org.springframework.util.Assert;
 @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public final class TmfApiFactory implements InitializingBean {
 	
-	private static final Logger log = LoggerFactory.getLogger(TmfApiFactory.class);
+	private static final Logger logger = LoggerFactory.getLogger(TmfApiFactory.class);
 	private static final String TMF_ENDPOINT_CONCAT_PATH = "-";
 	
     @Value("${tmforumapi.tmf_endpoint}")
@@ -42,55 +42,68 @@ public final class TmfApiFactory implements InitializingBean {
 	@Value( "${tmforumapi.tmf620_catalog_path}" )
 	private String tmf620CatalogPath;
 	
+	private it.eng.dome.tmforum.tmf637.v4.ApiClient apiClientTmf637;
+	private it.eng.dome.tmforum.tmf678.v4.ApiClient apiClientTmf678;
+	private it.eng.dome.tmforum.tmf620.v4.ApiClient apiClientTmf620;
+	
 	
 	public it.eng.dome.tmforum.tmf637.v4.ApiClient getTMF637ProductInventoryApiClient() {
-		final it.eng.dome.tmforum.tmf637.v4.ApiClient apiClient = it.eng.dome.tmforum.tmf637.v4.Configuration.getDefaultApiClient();
-		if (tmfEnvoy) {
-			// usage of envoyProxy to access on TMForum APIs (i.e. tmfEndpoint = http://tm-forum-api-envoy.marketplace.svc.cluster.local:8080)
-			apiClient.setBasePath(tmfEndpoint + "/" + tmf637ProductInventoryPath);
-		}else {
-			// use direct access on specific TMForum APIs software		
-			// tmfEndpoint is the prefix and you must append to the URL (using '-' char) the specific software (i.e. product-inventory)
-			apiClient.setBasePath(tmfEndpoint + TMF_ENDPOINT_CONCAT_PATH + "product-inventory" + "." + tmfNamespace + "." + tmfPostfix + ":" + tmfPort);
+		if (apiClientTmf637 == null) {
+			apiClientTmf637 = it.eng.dome.tmforum.tmf637.v4.Configuration.getDefaultApiClient(); 
+			if (tmfEnvoy) {
+				// usage of envoyProxy to access on TMForum APIs (i.e. tmfEndpoint = http://tm-forum-api-envoy.marketplace.svc.cluster.local:8080)
+				apiClientTmf637.setBasePath(tmfEndpoint + "/" + tmf637ProductInventoryPath);
+			}else {
+				// use direct access on specific TMForum APIs software		
+				// tmfEndpoint is the prefix and you must append to the URL (using '-' char) the specific software (i.e. product-inventory)
+				apiClientTmf637.setBasePath(tmfEndpoint + TMF_ENDPOINT_CONCAT_PATH + "product-inventory" + "." + tmfNamespace + "." + tmfPostfix + ":" + tmfPort);
+			}
+			logger.debug("Invoke Product Inventory API at endpoint: " + apiClientTmf637.getBasePath());
 		}
-		log.debug("Invoke Product Inventory API at endpoint: " + apiClient.getBasePath());
-		return apiClient;
+		
+		return apiClientTmf637;
 	}
 
 	public it.eng.dome.tmforum.tmf678.v4.ApiClient getTMF678CustomerBillApiClient() {
-		final it.eng.dome.tmforum.tmf678.v4.ApiClient apiClient = it.eng.dome.tmforum.tmf678.v4.Configuration.getDefaultApiClient();
-		if (tmfEnvoy) {
-			// usage of envoyProxy to access on TMForum APIs
-			apiClient.setBasePath(tmfEndpoint + "/" + tmf678CustomerBillPath);
-		}else {
-			// use direct access on specific TMForum APIs software	
-			apiClient.setBasePath(tmfEndpoint + TMF_ENDPOINT_CONCAT_PATH + "customer-bill-management" + "." + tmfNamespace + "." + tmfPostfix + ":" + tmfPort);		
+		if (apiClientTmf678 == null) { 
+			apiClientTmf678 = it.eng.dome.tmforum.tmf678.v4.Configuration.getDefaultApiClient();
+			if (tmfEnvoy) {
+				// usage of envoyProxy to access on TMForum APIs
+				apiClientTmf678.setBasePath(tmfEndpoint + "/" + tmf678CustomerBillPath);
+			}else {
+				// use direct access on specific TMForum APIs software	
+				apiClientTmf678.setBasePath(tmfEndpoint + TMF_ENDPOINT_CONCAT_PATH + "customer-bill-management" + "." + tmfNamespace + "." + tmfPostfix + ":" + tmfPort);		
+			}	
+			
+			logger.debug("Invoke Customer Billing API at endpoint: " + apiClientTmf678.getBasePath());
 		}
-		log.debug("Invoke Customer Billing API at endpoint: " + apiClient.getBasePath());
-		return apiClient;
+		return apiClientTmf678;
 	}
 	
 	public it.eng.dome.tmforum.tmf620.v4.ApiClient getTMF620CatalogApiClient() {
-		final it.eng.dome.tmforum.tmf620.v4.ApiClient apiClient = it.eng.dome.tmforum.tmf620.v4.Configuration.getDefaultApiClient();
-		if (tmfEnvoy) {
-			// usage of envoyProxy to access on TMForum APIs
-			apiClient.setBasePath(tmfEndpoint + "/" + tmf620CatalogPath);
-		}else {
-			// use direct access on specific TMForum APIs software
-			apiClient.setBasePath(tmfEndpoint + TMF_ENDPOINT_CONCAT_PATH + "product-catalog" + "." + tmfNamespace + "." + tmfPostfix + ":" + tmfPort);
-		}		
-		log.debug("Invoke Catalog API at endpoint: " + apiClient.getBasePath());
-		return apiClient;
+		if (apiClientTmf620 == null) {
+			apiClientTmf620  = it.eng.dome.tmforum.tmf620.v4.Configuration.getDefaultApiClient();
+			if (tmfEnvoy) {
+				// usage of envoyProxy to access on TMForum APIs
+				apiClientTmf620.setBasePath(tmfEndpoint + "/" + tmf620CatalogPath);
+			}else {
+				// use direct access on specific TMForum APIs software
+				apiClientTmf620.setBasePath(tmfEndpoint + TMF_ENDPOINT_CONCAT_PATH + "product-catalog" + "." + tmfNamespace + "." + tmfPostfix + ":" + tmfPort);
+			}		
+			logger.debug("Invoke Catalog API at endpoint: " + apiClientTmf620.getBasePath());
+		}
+		
+		return apiClientTmf620;
 	}
 	
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		
-		log.info("Billing Engine is using the following TMForum endpoint prefix: " + tmfEndpoint);
+		logger.info("Billing Engine is using the following TMForum endpoint prefix: " + tmfEndpoint);
 		if (tmfEnvoy) {
-			log.info("You set the apiProxy for TMForum endpoint. No tmf_port {} can be applied", tmfPort);	
+			logger.info("You set the apiProxy for TMForum endpoint. No tmf_port {} can be applied", tmfPort);	
 		} else {
-			log.info("No apiProxy set for TMForum APIs. You have to access on specific software via paths at tmf_port {}", tmfPort);	
+			logger.info("No apiProxy set for TMForum APIs. You have to access on specific software via paths at tmf_port {}", tmfPort);	
 		}
 				
 		Assert.state(!StringUtils.isBlank(tmfEndpoint), "Billing Scheduler not properly configured. tmf_endpoint property has no value.");
